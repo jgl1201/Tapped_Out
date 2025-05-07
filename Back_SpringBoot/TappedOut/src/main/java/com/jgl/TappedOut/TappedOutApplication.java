bbb812e3512bec3a5818e1f9a2674f22e2371013
Jorge Garcia <jgarcia@sealabsolutions.com>
2025-05-07 13:16:25 +0200

    create: Backend project at SpringBoot

---
+++ Back_SpringBoot/TappedOut/src/main/java/com/jgl/TappedOut/TappedOutApplication.java
@@ -0,0 +1,13 @@
+package com.jgl.TappedOut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TappedOutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TappedOutApplication.class, args);
+	}
+
+}
---
+++ Back_SpringBoot/TappedOut/src/test/java/com/jgl/TappedOut/TappedOutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgl.TappedOut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TappedOutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
