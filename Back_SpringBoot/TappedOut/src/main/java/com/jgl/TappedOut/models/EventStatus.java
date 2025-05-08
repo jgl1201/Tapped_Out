15e59830a7ae32e933e39ca2a0b8c253ff8de037
jgl1201 <jorgegarciacebem@gmail.com>
2025-05-08 16:40:27 +0200

    add: Event status enum

---
+++ Back_SpringBoot/TappedOut/src/main/java/com/jgl/TappedOut/models/EventStatus.java
@@ -0,0 +1,12 @@
+package com.jgl.TappedOut.models;
+
+/**
+ * Represents the possible stages for an event in the system
+ * 
+ * @author Jorge García López
+ * @version 1.0
+ * @since 2025
+ */
+public enum EventStatus {
+    PLANNED, ONGOING, COMPLETED, CANCELLED
+}
