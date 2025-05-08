56c7e09c611f967f0413139caaf97752f50bafc2
jgl1201 <jorgegarciacebem@gmail.com>
2025-05-08 16:42:06 +0200

    add: Inscription model

---
+++ Back_SpringBoot/TappedOut/src/main/java/com/jgl/TappedOut/models/Inscription.java
@@ -0,0 +1,60 @@
+package com.jgl.TappedOut.models;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+/**
+ * Represents an competitor's registration for an event category in the system
+ * Contains information about the registration date and payment status
+ * 
+ * @author Jorge García López
+ * @version 1.0
+ * @since 2025
+ */
+@Entity
+@Table(name = "inscriptions")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Inscription {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "Inscription COMPETITORID cannot be NULL")
+    @ManyToOne
+    @JoinColumn(name = "competitor_id", nullable = false)
+    private User competitorId;
+
+    @NotNull(message = "Inscription EVENTID cannot be NULL")
+    @ManyToOne
+    @JoinColumn(name = "event_id", nullable = false)
+    private Event event;
+
+    @NotNull(message = "Inscription CATEGORYID cannot be NULL")
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category categoryId;
+
+    @CreationTimestamp
+    @Column(name = "register_date", updatable = false)
+    private LocalDateTime registerDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "payment_status", columnDefinition = "ENUM('PENDING', 'PAID', 'CANCELLED') DEFAULT 'PENDING'")
+    private PaymentStatus paymentStatus = PaymentStatus.PENDING;
+}
