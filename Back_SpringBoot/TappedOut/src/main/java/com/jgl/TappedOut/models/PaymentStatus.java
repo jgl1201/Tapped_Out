6e01205c62e7d4e02333f6f1ca30a3b593ca5f82
jgl1201 <jorgegarciacebem@gmail.com>
2025-05-08 16:42:39 +0200

    add: Payment status enum

---
+++ Back_SpringBoot/TappedOut/src/main/java/com/jgl/TappedOut/models/PaymentStatus.java
@@ -0,0 +1,12 @@
+package com.jgl.TappedOut.models;
+
+/**
+ * Represents the possible stages for an inscription
+ * 
+ * @author Jorge García López
+ * @version 1.0
+ * @since 2025
+ */
+public enum PaymentStatus {
+    PENDING, PAID, CANCELLED
+}
