103bed4562b060ca1f91408f26bf2e1eb23163fe
jgl1201 <jorgegarciacebem@gmail.com>
2025-05-10 17:59:01 +0200

    add: User type repository

---
+++ Back_SpringBoot/TappedOut/src/main/java/com/jgl/TappedOut/repositories/UserTypeRepository.java
@@ -0,0 +1,21 @@
+package com.jgl.TappedOut.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jgl.TappedOut.models.UserType;
+
+/**
+ * Repository interface for managing {@link UserType} entities
+ * Provides basic CRUD operations
+ * 
+ * @author jorge García López
+ * @version 1.0
+ * @since 2025
+ */
+@Repository
+public interface UserTypeRepository extends JpaRepository<UserType, Long>{
+    boolean existsByName(String name);
+
+    UserType findByName(String name);
+}
