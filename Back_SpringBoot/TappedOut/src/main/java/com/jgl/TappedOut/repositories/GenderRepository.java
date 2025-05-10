5e143151838fca839e7e725848162b652df5ef04
jgl1201 <jorgegarciacebem@gmail.com>
2025-05-10 18:01:06 +0200

    add: Gender repository

---
+++ Back_SpringBoot/TappedOut/src/main/java/com/jgl/TappedOut/repositories/GenderRepository.java
@@ -0,0 +1,21 @@
+package com.jgl.TappedOut.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jgl.TappedOut.models.Gender;
+
+/**
+ * Repository interface for managing {@link Gender} entities
+ * Provides basic CRUD operations
+ * 
+ * @author Jorge García López
+ * @version 1.0
+ * @since 2025
+ */
+@Repository
+public interface GenderRepository extends JpaRepository<Gender, Long> {
+    boolean exisexistsByName(String name);
+
+    Gender findByName(String name);
+}
