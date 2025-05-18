013c420a67e7ef4df7170be1a4f1967359fcdc2f
jgl1201 <jorgegarciacebem@gmail.com>
2025-05-18 17:55:51 +0200

    add: DTO for Event response as json

---
+++ Back_SpringBoot/TappedOut/src/main/java/com/jgl/TappedOut/dto/EventResponseDTO.java
@@ -0,0 +1,35 @@
+package com.jgl.TappedOut.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Represents the response DTO for an event
+ * 
+ * @author Jorge García López
+ * @version 1.0
+ * @since 2025
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventResponseDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private String status;
+    private String country;
+    private String city;
+    private String address;
+    private String logo;
+    private String sport;
+    private String organizer;
+    private LocalDateTime createdAt;
+    private BigDecimal registrationFee;
+}
