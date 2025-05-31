ed0e09552b275ae6646608f84ccca57cf162cf50
Jorge Garcia <jorgegarciacebem@gmail.com>
2025-05-31 13:33:08 +0200

    add: DTO for User updating

---
+++ Back_SpringBoot/TappedOut/src/main/java/com/jgl/TappedOut/dto/UserUpdateDTO.java
@@ -0,0 +1,50 @@
+package com.jgl.TappedOut.dto;
+
+import java.time.LocalDate;
+
+import org.hibernate.validator.constraints.URL;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Represents a DTO for updating a user in the system
+ * 
+ * @author Jorge García López
+ * @version 1.0
+ * @since 2025
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserUpdateDTO {
+    @NotBlank(message = "Can't update a User without First Name")
+    @Size(max = 255, message = "User's first name's too long")
+    private String firstName;
+
+    @NotBlank(message = "Can't update a User without Last Name")
+    @Size(max = 255, message = "User's last name's too long")
+    private String lastName;
+
+    @NotNull(message = "Can't update a User without Date Of Birth")
+    @Past(message = "User's date o birth must be past")
+    private LocalDate dateOfBirth;
+
+    @NotBlank(message = "Can't update a User without Country")
+    private String country;
+
+    @NotBlank(message = "Can't update a User without City")
+    private String city;
+
+    private Integer phone;
+
+    @URL(message = "User's avatar must be an URL")
+    private String avatar;
+
+    private Boolean isVerified;
+}
