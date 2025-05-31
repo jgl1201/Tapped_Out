d710f5293eda047d40115583d44d1064442b0c4f
Jorge Garcia <jorgegarciacebem@gmail.com>
2025-05-31 13:10:56 +0200

    fix: Return of ResponseDTO instead of primitive types

--- Back_SpringBoot/TappedOut/src/main/java/com/jgl/TappedOut/dto/EventResponseDTO.java
+++ Back_SpringBoot/TappedOut/src/main/java/com/jgl/TappedOut/dto/EventResponseDTO.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
  * Represents the response DTO for an event
  * 
  * @author Jorge García López
- * @version 1.0
+ * @version 1.1
  * @since 2025
  */
 @Data
@@ -19,17 +19,30 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class EventResponseDTO {
     private Long id;
+
+    private UserResponseDTO organizer;
+
+    private SportResponseDTO sport;
+
     private String name;
+
     private String description;
+    
     private LocalDateTime startDate;
+
     private LocalDateTime endDate;
+    
     private String status;
+    
     private String country;
+
     private String city;
+
     private String address;
+
     private String logo;
-    private String sport;
-    private String organizer;
+
     private LocalDateTime createdAt;
+
     private BigDecimal registrationFee;
 }
