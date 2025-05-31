ba8531ab6beb1b94f7607347c5f96efe3b8170da
Jorge Garcia <jorgegarciacebem@gmail.com>
2025-05-31 13:03:30 +0200

    fix: Removed unused imports

--- Back_SpringBoot/TappedOut/src/main/java/com/jgl/TappedOut/dto/CategoryCreateDTO.java
+++ Back_SpringBoot/TappedOut/src/main/java/com/jgl/TappedOut/dto/CategoryCreateDTO.java
@@ -3,7 +3,6 @@ package com.jgl.TappedOut.dto;
 import java.math.BigDecimal;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
