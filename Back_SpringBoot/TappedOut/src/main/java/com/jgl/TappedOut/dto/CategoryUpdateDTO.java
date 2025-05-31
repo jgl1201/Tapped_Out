4cd922f23ed3c659435d3361b0fa930ad3265267
Jorge Garcia <jorgegarciacebem@gmail.com>
2025-05-31 13:06:07 +0200

    add: Response DTO for Category

---
+++ Back_SpringBoot/TappedOut/src/main/java/com/jgl/TappedOut/dto/CategoryUpdateDTO.java
@@ -0,0 +1,33 @@
+package com.jgl.TappedOut.dto;
+
+import java.math.BigDecimal;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Represents a DTO for returning a category entity to the user
+ * 
+ * @author Jorge García López
+ * @version 1.0
+ * @since 2025
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryUpdateDTO {
+    @NotBlank(message = "Can't update a Category without Name")
+    private String name;
+
+    private Integer minAge;
+    
+    private Integer maxAge;
+    
+    private BigDecimal minWeight;
+    
+    private BigDecimal maxWeight;
+
+    private Long levelId;
+}
