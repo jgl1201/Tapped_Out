c71333b11f157a6bde9cdb0db81817322ca6e6dd
Jorge Garcia <jorgegarciacebem@gmail.com>
2025-05-31 13:02:59 +0200

    fix: Clearer validation messages add: Size validation

--- Back_SpringBoot/TappedOut/src/main/java/com/jgl/TappedOut/dto/CategoryCreateDTO.java
+++ Back_SpringBoot/TappedOut/src/main/java/com/jgl/TappedOut/dto/CategoryCreateDTO.java
@@ -5,6 +5,7 @@ import java.math.BigDecimal;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,28 +14,29 @@ import lombok.NoArgsConstructor;
  * Represents a DTO for creating a new category in the system
  * 
  * @author Jorge García López
- * @version 1.0
+ * @version 1.1
  * @since 2025
  */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CategoryCreateDTO {
-    @NotNull (message = "Category SPORTID cannot be NULL")
+    @NotNull(message = "Can't create a Category without Sport")
     private Long sportId;
 
-    @NotNull (message = "Category NAME cannot be NULL")
-    @NotEmpty (message = "Category NAME cannot be EMPTY")
-    @NotBlank (message = "Category NAME cannot be BLANK")
+    @NotBlank(message = "Can't create a Category without Name")
+    @Size(max = 100, message = "Category's name's too long")
     private String name;
 
     private Integer minAge;
+
     private Integer maxAge;
 
     private BigDecimal minWeight;
+
     private BigDecimal maxWeight;
 
-    @NotNull (message = "Category GENDERID cannot be NULL")
+    @NotNull(message = "Can't create a Category without Gender")
     private Long genderId;
     
     private Long levelId;
