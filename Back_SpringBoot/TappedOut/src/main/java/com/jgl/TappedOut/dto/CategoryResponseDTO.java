b0e1cb08d49c798d70cbe559b5d57cdc6fa7bed6
Jorge Garcia <jorgegarciacebem@gmail.com>
2025-05-31 13:04:16 +0200

    fix: Return of ResponseDTO instead of primitive types

--- Back_SpringBoot/TappedOut/src/main/java/com/jgl/TappedOut/dto/CategoryResponseDTO.java
+++ Back_SpringBoot/TappedOut/src/main/java/com/jgl/TappedOut/dto/CategoryResponseDTO.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
  * Represents the response DTO for a category
  * 
  * @author Jorge García López
- * @version 1.0
+ * @version 1.1
  * @since 2025
  */
 @Data
@@ -18,14 +18,20 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CategoryResponseDTO {
     private Long id;
+    
     private String name;
 
     private Integer minAge;
+    
     private Integer maxAge;
+    
     private BigDecimal minWeight;
+    
     private BigDecimal maxWeight;
 
-    private String sport;
-    private String gender;
-    private String level;
+    private SportResponseDTO sport;
+    
+    private GenderResponseDTO gender;
+    
+    private SportLevelResponseDTO level;
 }
