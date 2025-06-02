cb8a1d4017eacc47516877c9c65d6480a659abfe
Jorge Garcia <jorgegarciacebem@gmail.com>
2025-06-02 19:17:48 +0200

    add: User Type mapper

---
+++ Back_SpringBoot/TappedOut/src/main/java/com/jgl/TappedOut/mapper/UserTypeMapper.java
@@ -0,0 +1,43 @@
+package com.jgl.TappedOut.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+import com.jgl.TappedOut.dto.UserTypeCreateDTO;
+import com.jgl.TappedOut.dto.UserTypeResponseDTO;
+import com.jgl.TappedOut.models.UserType;
+
+/**
+ * Mapper for converting between {@link UserType} entities and DTOs.
+ * Handles mapping including validations after it.
+ * 
+ * @author Jorge García López
+ * @version 1.1
+ * @since 2025
+ */
+@Mapper(componentModel = "spring")
+public abstract class UserTypeMapper {
+    /**
+     * Method to convert {@link UserTypeCreateDTO} into a {@link UserType}
+     * 
+     * @param dto the UserTypeCreateDTO to convert from
+     * @return the mapped UserType entity to persist
+     */
+    public abstract UserType fromCreateDTO(UserTypeCreateDTO dto);
+
+    /**
+     * Method to convert {@link UserType} into a {@link UserTypeResponseDTO}
+     * 
+     * @param userType the UserType to show
+     * @return the mapped response DTO
+     */
+    public abstract UserTypeResponseDTO toResponseDTO(UserType user);
+
+    /**
+     * Method to convert {@link UserTypeCreateDTO} into a {@link UserType}
+     * 
+     * @param dto the UserTypeCreateDTO to update from
+     * @param userType the entity UserType to be updated
+     */
+    public abstract void updateFromDTO(UserTypeCreateDTO dto, @MappingTarget UserType userType);
+}
