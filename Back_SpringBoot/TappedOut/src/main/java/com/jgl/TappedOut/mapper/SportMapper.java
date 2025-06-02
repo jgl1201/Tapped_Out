c90cb6d89a4905929781074b3de7717dd0091738
Jorge Garcia <jorgegarciacebem@gmail.com>
2025-06-02 19:18:04 +0200

    add: Sport mapper

---
+++ Back_SpringBoot/TappedOut/src/main/java/com/jgl/TappedOut/mapper/SportMapper.java
@@ -0,0 +1,43 @@
+package com.jgl.TappedOut.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+import com.jgl.TappedOut.dto.SportCreateDTO;
+import com.jgl.TappedOut.dto.SportResponseDTO;
+import com.jgl.TappedOut.models.Sport;
+
+/**
+ * Mapper for converting between {@link Result} entities and DTOs.
+ * Handles mapping including validations after it.
+ * 
+ * @author Jorge García López
+ * @version 1.0
+ * @since 2025
+ */
+@Mapper(componentModel = "spring")
+public abstract class SportMapper {
+    /**
+     * Method to convert {@link SportCreateDTO} into a {@link Sport}
+     * 
+     * @param dto the SportCreateDTO to convert from
+     * @return the mapped Gender entity to persist
+     */
+    public abstract Sport fromCreateDTO(SportCreateDTO dto);
+
+    /**
+     * Method to convert {@link Sport} into a {@link SportResponseDTO}
+     * 
+     * @param sport the Sport to show
+     * @return the mapped response DTO
+     */
+    public abstract SportResponseDTO toResponseDTO(Sport sport);
+
+    /**
+     * Method to convert {@link SportUpdateDTO} into a {@link Sport}
+     * 
+     * @param dto the SportUpdateDTO to update from
+     * @param sport the Sport entity to be updated
+     */
+    public abstract void updateFromDTO(SportCreateDTO dto, @MappingTarget Sport sport);
+}
