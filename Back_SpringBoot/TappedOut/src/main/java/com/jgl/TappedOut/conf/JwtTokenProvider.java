a9e232d542a3fe061bef678cf6b66e559ec67cd6
Jorge Garcia <jorgegarciacebem@gmail.com>
2025-06-19 10:10:19 +0200

    add: Class for providinbg JWT Tokens

---
+++ Back_SpringBoot/TappedOut/src/main/java/com/jgl/TappedOut/conf/JwtTokenProvider.java
@@ -0,0 +1,47 @@
+package com.jgl.TappedOut.conf;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+
+@Component
+public class JwtTokenProvider {
+    @Value("${app.jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${app.jwt.expiration}")
+    private int jwtExpiration;
+
+    public String generateToken(String username) {
+        return JWT.create()
+                .withSubject(username)
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + jwtExpiration))
+                .sign(Algorithm.HMAC512(jwtSecret.getBytes()));
+    }
+
+    public String getUsernameFromToken(String token) {
+        return JWT.require(Algorithm.HMAC512(jwtSecret))
+                .build()
+                .verify(token)
+                .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            JWT.require(Algorithm.HMAC512(jwtSecret))
+                    .build()
+                    .verify(token);
+            return true;
+        } catch (JWTVerificationException e) {
+            return false;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
