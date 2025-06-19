48742fddf7d3762fc6f9796d0ee5b4a75698d4e9
Jorge Garcia <jorgegarciacebem@gmail.com>
2025-06-19 15:09:30 +0200

    fix: Cyclic dependecy betwen services

--- Back_SpringBoot/TappedOut/src/main/java/com/jgl/TappedOut/service/InscriptionServiceImpl.java
+++ Back_SpringBoot/TappedOut/src/main/java/com/jgl/TappedOut/service/InscriptionServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -50,6 +51,7 @@ public class InscriptionServiceImpl implements InscriptionService {
     private UserServiceImpl userService;
 
     @Autowired
+    @Lazy
     private EventServiceImpl eventService;
 
     @Autowired
