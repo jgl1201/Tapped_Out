986355dc0887967d08d074f0acba3745031c74e0
Jorge Garcia <jorgegarciacebem@gmail.com>
2025-06-20 11:58:10 +0200

    fix: directory jeriarchy

--- Front_React/tappedout-front/src/App.jsx
+++ Front_React/tappedout-front/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import LoadingSpinner from './components/LoadingSpinner/LoadingSpiner'
+import LoadingSpinner from './components/LoadingSpiner'
 
 function App() {
   const [count, setCount] = useState(0)
