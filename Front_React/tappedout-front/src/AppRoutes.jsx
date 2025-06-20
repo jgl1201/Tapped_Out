37891e24ee7d6d45c20f43f611e9785ab66a3947
Jorge Garcia <jorgegarciacebem@gmail.com>
2025-06-20 16:59:59 +0200

    test: Change main view to check right petitions

--- Front_React/tappedout-front/src/AppRoutes.jsx
+++ Front_React/tappedout-front/src/AppRoutes.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+
+import LoadingSpinner from "./components/LoadingSpiner";
 import LoginForm from "./components/LoginForm";
 import RegisterForm from "./components/RegisterForm";
 
 const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/" element={<LoginForm />} />
+            <Route path="/" element={<LoadingSpinner />} />
             <Route path="/login" element={<LoginForm />} />
             <Route path="/register" element={<RegisterForm />} />
             <Route path="*" element={<Navigate to="/" />} />
