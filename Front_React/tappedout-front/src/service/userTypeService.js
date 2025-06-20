5cdeb893c00433317c34156d5c9675ce74bc56ee
Jorge Garcia <jorgegarciacebem@gmail.com>
2025-06-20 11:06:52 +0200

    add: User Type service for API petitions

---
+++ Front_React/tappedout-front/src/service/userTypeService.js
@@ -0,0 +1,64 @@
+/* eslint-disable no-useless-catch */
+import apiClient from "../api/apiClient";
+import { toast } from "react-hot-toast";
+
+const userTypeService = {
+    getAllUserTypes: async () => {
+        try {
+            const response = await apiClient.get('/user-types');
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    getUserTypeById: async (id) => {
+        try {
+            const response = await apiClient.get(`/user-types/${id}`);
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    getUserTypeByName: async (name) => {
+        try {
+            const response = await apiClient.get(`/user-types/name/${name}`);
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    createUserType: async (userTypeData) => {
+        try {
+            const response = await apiClient.post('/user-types', userTypeData);
+            toast.success('User type created successfully.');
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    updateUserType: async (id, userTypeData) => {
+        try {
+            const response = await apiClient.put(`/user-types/${id}`, userTypeData);
+            toast.success('User type updated successfully.');
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    deleteUserType: async (id) => {
+        try {
+            const response = await apiClient.delete(`/user-types/${id}`);
+            toast.success('User type deleted successfully.');
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    },
+};
+
+export default userTypeService;
