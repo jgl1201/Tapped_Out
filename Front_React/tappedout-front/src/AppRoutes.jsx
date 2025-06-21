cb0a7bf79bb494d287a64cd1a36527739f86a46c
Jorge Garcia <jorgegarciacebem@gmail.com>
2025-06-21 09:50:59 +0200

    feat: Implement test class for main view

--- Front_React/tappedout-front/src/AppRoutes.jsx
+++ Front_React/tappedout-front/src/AppRoutes.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
-import LoadingSpinner from "./components/LoadingSpiner";
 import LoginForm from "./components/LoginForm";
 import RegisterForm from "./components/RegisterForm";
+
+import Test from "./Test";
 
 const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/" element={<LoadingSpinner />} />
+            <Route path="/" element={<Test />} />
             <Route path="/login" element={<LoginForm />} />
             <Route path="/register" element={<RegisterForm />} />
             <Route path="*" element={<Navigate to="/" />} />
