ce1e805b444b94fd792c0334f1a311d84be53ab5
Jorge Garcia <jorgegarciacebem@gmail.com>
2025-06-21 16:22:39 +0200

    feat: Events creation

--- Front_React/tappedout-front/src/AppRoutes.jsx
+++ Front_React/tappedout-front/src/AppRoutes.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 
 import LoginForm from "./components/LoginForm";
 import RegisterForm from "./components/RegisterForm";
+import CreateEventForm from "./components/CreateEventForm";
 
 import Test from "./Test";
 
@@ -11,6 +12,7 @@ const AppRoutes = () => {
             <Route path="/" element={<Test />} />
             <Route path="/login" element={<LoginForm />} />
             <Route path="/register" element={<RegisterForm />} />
+            <Route path="/event/add" element={<CreateEventForm />} />
             <Route path="*" element={<Navigate to="/" />} />
         </Routes>
     );
