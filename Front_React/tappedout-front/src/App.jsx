61afcb7fb096c1af1f599d4354bb6711a3165977
Jorge Garcia <jorgegarciacebem@gmail.com>
2025-06-21 09:51:52 +0200

    fix: Better display feat: Toast persistency

--- Front_React/tappedout-front/src/App.jsx
+++ Front_React/tappedout-front/src/App.jsx
@@ -1,15 +1,25 @@
 import { BrowserRouter } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+
+import { Box } from '@mui/material'
+
 import AppRoutes from './AppRoutes'
 
 import Header from './components/Header'
-import { Box } from '@mui/material'
 
 function App() {
   return (
     <BrowserRouter>
-      <Box sx={{ minHeight: '100vh', width: '100vw', backgroundColor: '#121212' }}>
+    <Box sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+        width: '100vw',
+        backgroundColor: '#0d1117' }}
+      >
         <Header />
         <AppRoutes />
+        <ToastContainer />
       </Box>
     </BrowserRouter>
   )
